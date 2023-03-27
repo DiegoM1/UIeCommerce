42331ba373c08f6051abd3eeb3977f33cb1d8c6d
diegoM1 <diegomonteagudo@gmail.com>
2023-03-27 13:35:37 -0500

    Initial Commit

---
+++ Ecommerce TechProducts/Ecommerce_TechProductsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ecommerce_TechProductsApp.swift
+//  Ecommerce TechProducts
+//
+//  Created by Diego Monteagudo Diaz on 27/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Ecommerce_TechProductsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
